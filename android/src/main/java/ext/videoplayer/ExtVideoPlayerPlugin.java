e630e58bfec7c1fbdd281e1b79e3966e78808ed7
Sudhi S <sudhis@live.com>
2025-03-14 08:20:42 +0530

    fix: updated dependecies and update to run on latest flutter sdk

--- android/src/main/java/ext/videoplayer/ExtVideoPlayerPlugin.java
+++ android/src/main/java/ext/videoplayer/ExtVideoPlayerPlugin.java
@@ -1,4 +1,4 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2025 The ExtVideoPlayer Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,87 +12,52 @@ import io.flutter.Log;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.EventChannel;
-import ext.videoplayer.Messages.CreateMessage;
-import ext.videoplayer.Messages.LoopingMessage;
-import ext.videoplayer.Messages.MixWithOthersMessage;
-import ext.videoplayer.Messages.PlaybackSpeedMessage;
-import ext.videoplayer.Messages.PositionMessage;
-import ext.videoplayer.Messages.TextureMessage;
-import ext.videoplayer.Messages.VideoPlayerApi;
-import ext.videoplayer.Messages.VolumeMessage;
+import ext.videoplayer.Messages.*;
 import io.flutter.view.TextureRegistry;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import javax.net.ssl.HttpsURLConnection;
 
-/** Android platform implementation of the ExtVideoPlayerPlugin. */
+/** Android platform implementation of the ExtVideoPlayer plugin. */
 public class ExtVideoPlayerPlugin implements FlutterPlugin, VideoPlayerApi {
   private static final String TAG = "ExtVideoPlayerPlugin";
   private final LongSparseArray<VideoPlayer> videoPlayers = new LongSparseArray<>();
   private FlutterState flutterState;
   private VideoPlayerOptions options = new VideoPlayerOptions();
 
-  /** Register this with the v2 embedding for the plugin to respond to lifecycle callbacks. */
+  /** Default constructor for Flutter v2 embedding. */
   public ExtVideoPlayerPlugin() {}
-
-  @SuppressWarnings("deprecation")
-  private ExtVideoPlayerPlugin(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
-    this.flutterState =
-        new FlutterState(
-            registrar.context(),
-            registrar.messenger(),
-            registrar::lookupKeyForAsset,
-            registrar::lookupKeyForAsset,
-            registrar.textures());
-    flutterState.startListening(this, registrar.messenger());
-  }
-
-  /** Registers this with the stable v1 embedding. Will not respond to lifecycle events. */
-  @SuppressWarnings("deprecation")
-  public static void registerWith(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
-    final ExtVideoPlayerPlugin plugin = new ExtVideoPlayerPlugin(registrar);
-    registrar.addViewDestroyListener(
-        view -> {
-          plugin.onDestroy();
-          return false; // We are not interested in assuming ownership of the NativeView.
-        });
-  }
 
   @Override
   public void onAttachedToEngine(FlutterPluginBinding binding) {
-
-    if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
       try {
         HttpsURLConnection.setDefaultSSLSocketFactory(new CustomSSLSocketFactory());
       } catch (KeyManagementException | NoSuchAlgorithmException e) {
-        Log.w(
-            TAG,
-            "Failed to enable TLSv1.1 and TLSv1.2 Protocols for API level 19 and below.\n"
-                + "For more information about Socket Security, please consult the following link:\n"
-                + "https://developer.android.com/reference/javax/net/ssl/SSLSocket",
-            e);
+        Log.w(TAG, "Failed to enable TLSv1.1 and TLSv1.2 for older APIs.", e);
       }
     }
 
-    final FlutterInjector injector = FlutterInjector.instance();
-    this.flutterState =
-        new FlutterState(
+    FlutterInjector injector = FlutterInjector.instance();
+    this.flutterState = new FlutterState(
             binding.getApplicationContext(),
             binding.getBinaryMessenger(),
             injector.flutterLoader()::getLookupKeyForAsset,
             injector.flutterLoader()::getLookupKeyForAsset,
-            binding.getTextureRegistry());
+            binding.getTextureRegistry()
+    );
     flutterState.startListening(this, binding.getBinaryMessenger());
   }
 
   @Override
   public void onDetachedFromEngine(FlutterPluginBinding binding) {
     if (flutterState == null) {
-      Log.wtf(TAG, "Detached from the engine before registering to it.");
+      Log.w(TAG, "Detached from engine before initialization.");
+      return;
     }
     flutterState.stopListening(binding.getBinaryMessenger());
     flutterState = null;
-    initialize();
+    disposeAllPlayers();
   }
 
   private void disposeAllPlayers() {
@@ -102,53 +67,11 @@ public class ExtVideoPlayerPlugin implements FlutterPlugin, VideoPlayerApi {
     videoPlayers.clear();
   }
 
-  private void onDestroy() {
-    // The whole FlutterView is being destroyed. Here we release resources acquired for all
-    // instances
-    // of VideoPlayer. Once https://github.com/flutter/flutter/issues/19358 is resolved this may
-    // be replaced with just asserting that videoPlayers.isEmpty().
-    // https://github.com/flutter/flutter/issues/20989 tracks this.
-    disposeAllPlayers();
-  }
-
-  public void initialize() {
-    disposeAllPlayers();
-  }
-
   public TextureMessage create(CreateMessage arg) {
-    TextureRegistry.SurfaceTextureEntry handle =
-        flutterState.textureRegistry.createSurfaceTexture();
-    EventChannel eventChannel =
-        new EventChannel(
-            flutterState.binaryMessenger, "flutter.io/videoPlayer/videoEvents" + handle.id());
+    TextureRegistry.SurfaceTextureEntry handle = flutterState.textureRegistry.createSurfaceTexture();
+    EventChannel eventChannel = new EventChannel(flutterState.binaryMessenger, "flutter.io/videoPlayer/videoEvents" + handle.id());
 
-    VideoPlayer player;
-    if (arg.getAsset() != null) {
-      String assetLookupKey;
-      if (arg.getPackageName() != null) {
-        assetLookupKey =
-            flutterState.keyForAssetAndPackageName.get(arg.getAsset(), arg.getPackageName());
-      } else {
-        assetLookupKey = flutterState.keyForAsset.get(arg.getAsset());
-      }
-      player =
-          new VideoPlayer(
-              flutterState.applicationContext,
-              eventChannel,
-              handle,
-              "asset:///" + assetLookupKey,
-              null,
-              options);
-    } else {
-      player =
-          new VideoPlayer(
-              flutterState.applicationContext,
-              eventChannel,
-              handle,
-              arg.getUri(),
-              arg.getFormatHint(),
-              options);
-    }
+    VideoPlayer player = (arg.getAsset() != null) ? createAssetPlayer(arg, eventChannel, handle) : createUriPlayer(arg, eventChannel, handle);
     videoPlayers.put(handle.id(), player);
 
     TextureMessage result = new TextureMessage();
@@ -156,61 +79,80 @@ public class ExtVideoPlayerPlugin implements FlutterPlugin, VideoPlayerApi {
     return result;
   }
 
+  private VideoPlayer createAssetPlayer(CreateMessage arg, EventChannel eventChannel, TextureRegistry.SurfaceTextureEntry handle) {
+    String assetLookupKey = (arg.getPackageName() != null) ?
+            flutterState.keyForAssetAndPackageName.get(arg.getAsset(), arg.getPackageName()) :
+            flutterState.keyForAsset.get(arg.getAsset());
+    return new VideoPlayer(flutterState.applicationContext, eventChannel, handle, "asset:///" + assetLookupKey, null, options);
+  }
+
+  private VideoPlayer createUriPlayer(CreateMessage arg, EventChannel eventChannel, TextureRegistry.SurfaceTextureEntry handle) {
+    return new VideoPlayer(flutterState.applicationContext, eventChannel, handle, arg.getUri(), arg.getFormatHint(), options);
+  }
+
   public void dispose(TextureMessage arg) {
     VideoPlayer player = videoPlayers.get(arg.getTextureId());
-    player.dispose();
-    videoPlayers.remove(arg.getTextureId());
+    if (player != null) {
+      player.dispose();
+      videoPlayers.remove(arg.getTextureId());
+    }
   }
 
   public void setLooping(LoopingMessage arg) {
     VideoPlayer player = videoPlayers.get(arg.getTextureId());
-    player.setLooping(arg.getIsLooping());
+    if (player != null) {
+      player.setLooping(arg.getIsLooping());
+    }
   }
 
   public void setVolume(VolumeMessage arg) {
     VideoPlayer player = videoPlayers.get(arg.getTextureId());
-    player.setVolume(arg.getVolume());
+    if (player != null) {
+      player.setVolume(arg.getVolume());
+    }
   }
 
   public void setPlaybackSpeed(PlaybackSpeedMessage arg) {
     VideoPlayer player = videoPlayers.get(arg.getTextureId());
-    player.setPlaybackSpeed(arg.getSpeed());
+    if (player != null) {
+      player.setPlaybackSpeed(arg.getSpeed());
+    }
   }
 
   public void play(TextureMessage arg) {
     VideoPlayer player = videoPlayers.get(arg.getTextureId());
-    player.play();
+    if (player != null) {
+      player.play();
+    }
   }
 
   public PositionMessage position(TextureMessage arg) {
     VideoPlayer player = videoPlayers.get(arg.getTextureId());
     PositionMessage result = new PositionMessage();
-    result.setPosition(player.getPosition());
-    player.sendBufferingUpdate();
+    if (player != null) {
+      result.setPosition(player.getPosition());
+      player.sendBufferingUpdate();
+    }
     return result;
   }
 
   public void seekTo(PositionMessage arg) {
     VideoPlayer player = videoPlayers.get(arg.getTextureId());
-    player.seekTo(arg.getPosition().intValue());
+    if (player != null) {
+      player.seekTo(arg.getPosition().intValue());
+    }
   }
 
   public void pause(TextureMessage arg) {
     VideoPlayer player = videoPlayers.get(arg.getTextureId());
-    player.pause();
+    if (player != null) {
+      player.pause();
+    }
   }
 
   @Override
   public void setMixWithOthers(MixWithOthersMessage arg) {
     options.mixWithOthers = arg.getMixWithOthers();
-  }
-
-  private interface KeyForAssetFn {
-    String get(String asset);
-  }
-
-  private interface KeyForAssetAndPackageName {
-    String get(String asset, String packageName);
   }
 
   private static final class FlutterState {
@@ -220,12 +162,7 @@ public class ExtVideoPlayerPlugin implements FlutterPlugin, VideoPlayerApi {
     private final KeyForAssetAndPackageName keyForAssetAndPackageName;
     private final TextureRegistry textureRegistry;
 
-    FlutterState(
-        Context applicationContext,
-        BinaryMessenger messenger,
-        KeyForAssetFn keyForAsset,
-        KeyForAssetAndPackageName keyForAssetAndPackageName,
-        TextureRegistry textureRegistry) {
+    FlutterState(Context applicationContext, BinaryMessenger messenger, KeyForAssetFn keyForAsset, KeyForAssetAndPackageName keyForAssetAndPackageName, TextureRegistry textureRegistry) {
       this.applicationContext = applicationContext;
       this.binaryMessenger = messenger;
       this.keyForAsset = keyForAsset;
@@ -233,8 +170,8 @@ public class ExtVideoPlayerPlugin implements FlutterPlugin, VideoPlayerApi {
       this.textureRegistry = textureRegistry;
     }
 
-    void startListening(ExtVideoPlayerPlugin methodCallHandler, BinaryMessenger messenger) {
-      VideoPlayerApi.setup(messenger, methodCallHandler);
+    void startListening(ExtVideoPlayerPlugin handler, BinaryMessenger messenger) {
+      VideoPlayerApi.setup(messenger, handler);
     }
 
     void stopListening(BinaryMessenger messenger) {
